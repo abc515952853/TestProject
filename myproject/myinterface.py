532d44d74cffc5a7c2aefaa016aca876c0635bb2
abc515952853 <123456789@qq.com>
2018-05-26 21:25:16 +0800

    登录

--- myproject/myinterface.py
+++ myproject/myinterface.py
@@ -2,36 +2,42 @@ import requests
 import  json  
 
 def test():
-    # phone = '18506826613'
-    # mtjid = 'HHX666666'
-    # #生成学员编号
-    # r = requests.get(url='http://api.hhx.qianjifang.com.cn/api/Account')
-    # mnickname = r.json()
+    phone = '18506826614'
+    mtjid = '888888'
+    #生成学员编号
+    r = requests.get(url='http://api.hhx.qianjifang.com.cn/api/Account')
+    mnickname = r.json()
 
-    # #发送验证码
-    # payload = {"phone":phone,"type":1}
-    # headers = {"Content-Type":"application/json"}  
-    # r = requests.post(url='http://api.hhx.qianjifang.com.cn/api/Account/SendCode',data = json.dumps(payload),headers = headers) 
-    # print('发送验证码',r.status_code,r.text)
+    #发送验证码
+    payload = {"phone":phone,"type":1}
+    headers = {"Content-Type":"application/json"}  
+    r = requests.post(url='http://api.hhx.qianjifang.com.cn/api/Account/SendCode',data = json.dumps(payload),headers = headers) 
+    print('发送验证码',r.status_code,r.text)
 
-    # #注册
-    # payload = {"Number": mnickname, "TjNumber": mtjid,"phone":phone,"code":"1234"}
-    # headers = {"Content-Type":"application/json"} 
-    # r = requests.post(url='http://api.hhx.qianjifang.com.cn/api/Account/Register',data = json.dumps(payload),headers = headers) 
-    # print('注册',r.status_code,r.text)
+    #注册
+    payload = {"Number": mnickname, "TjNumber": mtjid,"185":phone,"code":"1234"}
+    headers = {"Content-Type":"application/json"} 
+    r = requests.post(url='http://api.hhx.qianjifang.com.cn/api/Account/Register',data = json.dumps(payload),headers = headers) 
+    print('注册',r.status_code,r.text)
 
-    #登录
-    payload = {"grant_type":'code', "username": 'HHX017865',"password":"123456"}
-    r = requests.post(url='http://api.hhx.qianjifang.com.cn/api/Token', data = payload)
-    session = r.json()["token_type"]+" "+r.json()["access_token"]
-    print(r.status_code,r.text)
+    # #登录
+    # payload = {"grant_type":'code', "username": mnickname,"password":"123456"}
+    # r = requests.post(url='http://api.hhx.qianjifang.com.cn/api/Token', data = payload)
+    # session = r.json()["token_type"]+" "+r.json()["access_token"]
+    # print(r.status_code,r.text)
 
+    
 
-    headers = {"Content-Type":"application/json","Authorization":session} 
-    r = requests.get(url='http://api.hhx.qianjifang.com.cn/api/Home',headers = headers)
-    print(r.status_code,r.json())
+    # #登录
+    # payload = {"grant_type":'code', "username": 'HHX017865',"password":"123456"}
+    # r = requests.post(url='http://api.hhx.qianjifang.com.cn/api/Token', data = payload)
+    # session = r.json()["token_type"]+" "+r.json()["access_token"]
+    # print(r.status_code,r.text)
 
-
+    # #首页
+    # headers = {"Content-Type":"application/json","Authorization":session} 
+    # r = requests.get(url='http://api.hhx.qianjifang.com.cn/api/Home',headers = headers)
+    # print(r.status_code,r.json())
 
 
     #忘记密码
